1f107830b3868c08ca5d40da4aca671e9ad385c1
rcdzo <ricardo.vivosempre@gmail.com>
2023-06-19 19:52:49 -0300

    ajuste Domain Restaurant

--- src/main/kotlin/com/br/geolocation/application/entities/Restaurant.kt
+++ src/main/kotlin/com/br/geolocation/application/entities/Restaurant.kt
@@ -1,9 +1,11 @@
 package com.br.geolocation.application.entities
 
+import com.br.geolocation.application.valueobjects.Location
 import com.br.geolocation.application.valueobjects.Menu
 
 data class Restaurant(
     val id: String,
+    val location: Location,
     val name: String,
     val menu: List<Menu>
 )
